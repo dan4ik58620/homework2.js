43f2d90437f618a489232648d53bd926175609fd
dan4ik58620 <56477594+dan4ik58620@users.noreply.github.com>
2020-03-06 22:03:19 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+"use strict";
+let Генератор_защитного_поля= true;
+console.log(Генератор_защитного_поля)
+console.log(typeof Генератор_защитного_поля)
+Генератор_защитного_поля=1000;
+console.log(Генератор_защитного_поля)
+console.log(typeof Генератор_защитного_поля_куплен_за_1000_кридитов )
+
+const userName = "приветствие";
+console.log(userName);
+console.log( typeof userName);
+
+let mon ="Имя пользователя";
+console.log(mon);
+console.log(`${mon}, ${userName}`);
+prompt(`${mon}, ${userName}`);
+
+
+const price = "цена за штуку 1000 кредитов";
+console.log(price);
+console.log( typeof price);
+
+let men ="Выбран «Генератор защитного поля»";
+console.log(men);
+console.log(`${men}, ${price}`);
+confirm(`${men}, ${price}`);
+
+const ubgrade = "цена за штуку 2000 кредитов";
+console.log(ubgrade);
+console.log( typeof ubgrade);
+
+let min ="Выбран «Генератор защитного поля»";
+console.log(min);
+console.log(`${min}, ${ubgrade}`);
+confirm(`${min}, ${ubgrade}`);
+
